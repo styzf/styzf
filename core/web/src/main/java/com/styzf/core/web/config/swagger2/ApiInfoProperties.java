9bcb2710f71c5809e59fc6c747f7b02f118901f1
styzf <styzfa@163.com>
2018-08-04 13:09:53 +0800

    swagger2配置类

---
+++ core/web/src/main/java/com/styzf/core/web/config/swagger2/ApiInfoProperties.java
@@ -0,0 +1,79 @@
+package com.styzf.core.web.config.swagger2;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix="styzf.apiInfo")
+public class ApiInfoProperties {
+    private String version;
+    private String title;
+    private String description;
+    private String termsOfServiceUrl;
+    private String license;
+    private String licenseUrl;
+    private String pathsRegex;
+    private Contact contact;
+    
+    public String getVersion() {
+        return version;
+    }
+    
+    public String getTitle() {
+        return title;
+    }
+    
+    public String getDescription() {
+        return description;
+    }
+    
+    public String getTermsOfServiceUrl() {
+        return termsOfServiceUrl;
+    }
+    
+    public String getLicense() {
+        return license;
+    }
+    
+    public String getLicenseUrl() {
+        return licenseUrl;
+    }
+    
+    public Contact getContact() {
+        return contact;
+    }
+    
+    public String getPathsRegex() {
+        return pathsRegex;
+    }
+    
+    public void setPathsRegex(String pathsRegex) {
+        this.pathsRegex = pathsRegex;
+    }
+    
+    public void setVersion(String version) {
+        this.version = version;
+    }
+    
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+    public void setTermsOfServiceUrl(String termsOfServiceUrl) {
+        this.termsOfServiceUrl = termsOfServiceUrl;
+    }
+    
+    public void setLicense(String license) {
+        this.license = license;
+    }
+    
+    public void setLicenseUrl(String licenseUrl) {
+        this.licenseUrl = licenseUrl;
+    }
+    
+    public void setContact(Contact contact) {
+        this.contact = contact;
+    }
+}
---
+++ core/web/src/main/java/com/styzf/core/web/config/swagger2/Swagger2Config.java
@@ -0,0 +1,36 @@
+package com.styzf.core.web.config.swagger2;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.annotations.ApiOperation;
+import springfox.documentation.annotations.ApiIgnore;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+@EnableConfigurationProperties({ApiInfoProperties.class})
+public class Swagger2Config {
+    @Bean
+    public Docket springfoxDocket(ApiInfoProperties apiInfo) {
+        Docket docket = new Docket(DocumentationType.SWAGGER_2);
+        docket.ignoredParameterTypes(new Class[] { ApiIgnore.class });
+        docket.apiInfo(apiInfo(apiInfo));
+        docket.pathMapping("/").select().paths(PathSelectors.regex("^.*(?<!error)$")).build();
+        docket.select().apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class)).build();
+        return docket;
+    }
+    
+    @Bean
+    ApiInfo apiInfo(ApiInfoProperties apiInfoProperties) {
+        Contact contact = apiInfoProperties.getContact();
+        ApiInfo apiInfo = new ApiInfo(apiInfoProperties.getTitle(), apiInfoProperties.getDescription(), apiInfoProperties.getVersion(), apiInfoProperties.getTermsOfServiceUrl(), new springfox.documentation.service.Contact(contact.getName(), contact.getUrl(), contact.getEmail()), apiInfoProperties.getLicense(), apiInfoProperties.getLicenseUrl());
+        return apiInfo;
+    }
+}
